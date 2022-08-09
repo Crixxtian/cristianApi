a2dc4dbd0cf1ddd5f925751d48eddf2554ab49c7
Cristian Machado <cristian.machado4@etec.sp.gov.br>
2022-08-09 09:47:33 -0300

    Initial Commit

---
+++ src/main/java/br/com/cristian/cristianApi/CristianApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.cristian.cristianApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CristianApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CristianApiApplication.class, args);
+	}
+
+}
